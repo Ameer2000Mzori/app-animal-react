0ae13efb1571334654e46aae4b134752dc9353ea
Ameer Ameen <ameerapex496@gmail.com>
2024-02-13 04:41:13 +0100

    added like logic for each card has its own like and that like can be triggered

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,14 +2,33 @@ import { useRef } from 'react'
 import heart from '../assets/heart.svg'
 const Card = (props) => {
   const addLikeRef = useRef(null)
+
+  let heartHight = 25
   const addLike = () => {
-    console.log('addLike')
+    if (addLikeRef.current) {
+      if (heartHight < 200) {
+        heartHight += 25
+        addLikeRef.current.style.height = `${heartHight}px`
+        addLikeRef.current.style.width = `${heartHight}px`
+        console.log('addLike')
+      } else {
+        heartHight = 25
+      }
+    } else {
+      console.log('not addLikeRef')
+    }
   }
   return (
     <div>
       <img src={props.img} alt="" />
       <p>{props.name}</p>
-      <img ref={addLikeRef} onClick={addLike} src={heart} alt="" />
+      <img
+        className="h-[50px] w-[50px]"
+        ref={addLikeRef}
+        onClick={addLike}
+        src={heart}
+        alt=""
+      />
     </div>
   )
 }
