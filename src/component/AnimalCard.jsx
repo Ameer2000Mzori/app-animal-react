78e2e7e4d580be77fee75d6f010366dedd4eed2f
Ameer Ameen <ameerapex496@gmail.com>
2024-02-13 04:15:21 +0100

    added more logic where to list animal and each has its own like btn

--- src/component/AnimalCard.jsx
+++ src/component/AnimalCard.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 import Card from './Card'
 
 const AnimalCard = () => {
-  return <Card />
+  const animalObjs = [
+    {
+      name: 'Dog',
+      img: 'dog.png',
+    },
+    {
+      name: 'cow',
+      img: 'cow.png',
+    },
+  ]
+
+  return (
+    <>
+      <button className="w-[100px] h-[30px] bg-black text-white">
+        add animal
+      </button>
+      {animalObjs.map((animal, index) => (
+        <Card key={index} name={animal.name} img={animal.img} />
+      ))}
+    </>
+  )
 }
 
 export default AnimalCard
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,14 +1,15 @@
 import React, { useRef } from 'react'
-
-const Card = () => {
+import heart from '../assets/heart.svg'
+const Card = (props) => {
   const addLikeRef = useRef(null)
   const addLike = () => {
     console.log('addLike')
   }
   return (
     <div>
-      <img src="" alt="" />
-      <img ref={addLikeRef} onClick={addLike} src="" alt="" />
+      <img src={props.img} alt="" />
+      <p>{props.name}</p>
+      <img ref={addLikeRef} onClick={addLike} src={heart} alt="" />
     </div>
   )
 }
